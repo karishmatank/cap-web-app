f7be284ad9091275ea04db5476e53c8fb3e79d5a
Karishma Tank <tankkarishma@gmail.com>
2025-03-23 17:03:55 -0400

    Adds user creation and changing passwords

--- cap_portal/users/urls.py
+++ cap_portal/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -6,5 +7,10 @@ app_name = "users"
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout")
+    path("logout", views.logout_view, name="logout"),
+    path("create", views.create_user, name="create"),
+    path("change-password", auth_views.PasswordChangeView.as_view(
+        template_name="users/change_password.html", 
+        success_url="login"
+    ), name="change_password")
 ]
--- cap_portal/users/views.py
+++ cap_portal/users/views.py
@@ -1,9 +1,45 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 # Create your views here.
+def create_user(request):
+    if request.method == 'POST':
+        # Check if there is already a user associated with the email
+        user_exists = User.objects.filter(email=request.POST["email"]).exists()
+        if user_exists:
+            return render(request, "users/create_user.html", {
+                "message": f"Account already exists for email address {request.POST["email"]}."
+            })
+
+        # Check if username is already in use
+        username_exists = User.objects.filter(username=request.POST["username"]).exists()
+        if username_exists:
+            return render(request, "users/create_user.html", {
+                "message": f"Username {request.POST["username"]} is already in use."
+            })
+        
+        # Check if both password entries match
+        if request.POST["password"] != request.POST["password_reentry"]:
+            return render(request, "users/create_user.html", {
+                "message": f"Passwords do not match."
+            })
+        
+        # If everything looks good, create a new user
+        new_user = User.objects.create_user(
+            request.POST["username"],
+            request.POST["email"],
+            request.POST["password"]
+        )
+
+        return render(request, "users/login.html", {
+            "message": "Account created, please log in!"
+        })
+
+    return render(request, "users/create_user.html")
+
 def index(request):
     # If the user isn't signed in, redirect to the login page
     if not request.user.is_authenticated:
