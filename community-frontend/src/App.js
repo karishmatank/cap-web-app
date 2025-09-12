5aa9beb2dc01c6a22b5ec4c4b7086a52f8aca292
Karishma Tank <tankkarishma@gmail.com>
2025-09-12 14:44:45 -0400

    [Directory] Adds default mentor list view when user first loads

--- community-frontend/src/App.js
+++ community-frontend/src/App.js
@@ -5,21 +5,43 @@ import Navbar from './components/Navbar';
 import Searchbar from './components/Searchbar';
 import DirectoryResults from './components/DirectoryResults';
 import useCurrentUser from './hooks/useCurrentUser';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import PushInitializer from './components/PushInitializer';
 
 function Layout() {
   const [query, setQuery] = useState("");
   const [role, setRole] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [mentorResults, setMentorResults] = useState([]);
+  const didInitialFetch = useRef(false);
   const { currentUser, loading } = useCurrentUser();
+
+  // Let's get an initial list of results to show on the screen so it's not just a blank page
+  // Instead of pulling everyone's info, I'll stick to just mentors
+  useEffect(() => {
+    if (didInitialFetch.current) return;
+
+    didInitialFetch.current = true;
+
+    axios.get('/users/api/community-search/?q=&role=mentor')
+    .then((response) => {
+      setMentorResults(response.data);
+    })
+    .catch((error) => {
+      if (error.name ==='CanceledError' || error.code === "ERR_CANCELED") {
+        // Request was cancelled, ignore
+        return;
+      }
+      console.error("Error getting initial community search results", error);
+    });
+  }, [currentUser, loading]);
 
   /* Whenever query changes, we'll call the API to get results */
   useEffect(() => {
     const controller = new AbortController(); // For when we backspace too quickly and the API still shows results
 
     if (query.length < 2) {
-      setSearchResults([]);
+      setSearchResults(mentorResults);
       return;
     }
 
@@ -41,7 +63,7 @@ function Layout() {
       clearTimeout(delay);
       controller.abort();
     };
-  }, [query, role]);
+  }, [query, role, mentorResults]);
 
   /* Check that there is a currentUser (user is authenticated) 
   Need to keep this for last because otherwise React freaks out about hooks not running in the same order every render
